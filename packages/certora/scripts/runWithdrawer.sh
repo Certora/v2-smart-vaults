5f1ac3059a9399b65f4fcfbdd705aee8b2796b3b
Yuval Zalmenson <yuvalzalmenson@Yuvals-MacBook-Pro-3.local>
2023-04-16 11:00:47 +0300

    Linking the Withdrawer contract with SymbolicSmartVault

--- packages/certora/scripts/runWithdrawer.sh
+++ packages/certora/scripts/runWithdrawer.sh
@@ -1,13 +1,12 @@
 certoraRun ./packages/balancer-fee-collector/contracts/actions/withdraw/Withdrawer.sol \
-    ./node_modules/@mimic-fi/v2-smart-vault/contracts/SmartVault.sol \
+    ./packages/certora/helpers/SymbolicSmartVault.sol \
     ./packages/certora/helpers/ProtocolFeeWithdrawerMock.sol \
     ./node_modules/@mimic-fi/v2-price-oracle/contracts/oracle/PriceOracle.sol \
     ./packages/certora/helpers/ERC20_A.sol \
     ./packages/certora/helpers/ERC20_B.sol \
 --verify Withdrawer:./packages/certora/specs/Withdrawer.spec \
 --link \
-    Withdrawer:smartVault=SmartVault \
-    SmartVault:priceOracle=PriceOracle \
+    Withdrawer:smartVault=SymbolicSmartVault \
 --packages @openzeppelin=node_modules/@openzeppelin @mimic-fi=node_modules/@mimic-fi @chainlink=node_modules/@chainlink \
 --path . \
 --send_only \
@@ -19,3 +18,4 @@ certoraRun ./packages/balancer-fee-collector/contracts/actions/withdraw/Withdraw
 
 
 #sanity https://prover.certora.com/output/47234/32a33e9084d948b2bb8f036758cf1f20?anonymousKey=0360c117d26be751f909a61b72a298d2c7d9d70a
+#sanity w SymbolicSmartVault  https://prover.certora.com/output/47234/33a956298ba242e0b395bd2814572248?anonymousKey=64ec9ddb437f818d4adc3440ae115b956b6af910
